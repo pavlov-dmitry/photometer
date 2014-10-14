8f5bcba11e348d0d819b44a3cf5074e3d31e5a32
pavlov-dmitry <voidwalker@mail.ru>
2014-10-14 05:00:25 -0700

    В конфиг добавлены параметры для подключения к БД

--- src/config.rs
+++ src/config.rs
@@ -4,14 +4,20 @@ use serialize::json;
 #[deriving(Encodable, Decodable)]
 pub struct Config {
 	pub static_files_path : String,
-	pub login_page_path : String
+	pub login_page_path : String,
+	pub db_name : String,
+	pub db_user_name : String,
+	pub db_user_password : String
 }
 
 impl Config {
 	fn new() -> Config {
 		Config{ 
 			login_page_path : "../www/login.html".to_string(),
-			static_files_path : "../www/".to_string()
+			static_files_path : "../www/".to_string(),
+			db_name : "photometer".to_string(),
+			db_user_name : "photometer".to_string(),
+			db_user_password : "parol".to_string()
 		}
 	}
 }
