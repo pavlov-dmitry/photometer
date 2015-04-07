80e06970bd032a829491dc354ed4dbf1ea9713e8
pavlov-dmitry <voidwalker@mail.ru>
2015-04-07 01:27:40 -0700

    (front-end) Теперь и выпадющее меню в навигации тоже тёмное

--- www/template/user_state_header_view.js
+++ www/template/user_state_header_view.js
@@ -5,7 +5,7 @@ return templates['user_state_header_view'] = template({"1":function(depth0,helpe
 
   return "  <li class=\"dropdown\">\n    <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-expanded=\"false\">\n      <span class=\"glyphicon glyphicon-user\" ></span> "
     + this.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
-    + " <span class=\"caret\"></span>\n    </a>\n    <ul class=\"dropdown-menu\" role=\"menu\">\n      <li class=\"devider\"></li>\n      <li><a id=\"logout-action\" href=\"#\" onClick=\"return false\"><span class=\"glyphicon glyphicon-off\"></span> Выйти</a></li>\n      <!-- <li><button type=\"button\" class=\"navbar-btn\"><span class=\"glyphicon glyphicon-off\"> Выйти</button></li> -->\n    </ul>\n  </li>\n";
+    + " <span class=\"caret\"></span>\n    </a>\n    <ul class=\"dropdown-menu inverse-dropdown\" role=\"menu\">\n      <li><a id=\"logout-action\" href=\"#\" onClick=\"return false\"><span class=\"glyphicon glyphicon-off\"></span> Выйти</a></li>\n      <!-- <li><button type=\"button\" class=\"navbar-btn\"><span class=\"glyphicon glyphicon-off\"> Выйти</button></li> -->\n    </ul>\n  </li>\n";
 },"3":function(depth0,helpers,partials,data) {
     return "  <a href=\"#login\"><button type=\"button\" class=\"btn btn-success navbar-btn\">Войти</button></a>\n  <div class=\"nav navbar-nav spacer-w-sm\"></div>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
