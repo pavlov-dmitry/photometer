097eabcebfa1ad4042df88e7a9338ef0861321aa
pavlov-dmitry <voidwalker@mail.ru>
2015-04-17 03:42:51 -0700

    (front-end) Реализовал переименование фотографии

--- www/js/edit_photo/edit_view.js
+++ www/js/edit_photo/edit_view.js
@@ -5,20 +5,46 @@ define( function( require ) {
 
     var EditView = Backbone.View.extend({
 
+        el: "#workspace",
+
         template: Handlebars.templates.photo_edit_view,
 
         events: {
+            "submit #rename-photo-form": "rename",
         },
 
         initialize: function() {
-            this.el = $( "#workspace" );
             this.model.on( "all", this.render, this );
             this.model.fetch();
         },
 
         render: function() {
-            this.el.html( this.template( this.model.toJSON() ) );
+            this.$el.html( this.template( this.model.toJSON() ) );
             return this;
+        },
+
+        rename: function() {
+            var request = require( "request" );
+            var handler = request.post( "/rename", {
+                id: this.model.get( "id" ),
+                name: $("#new-name-input").val()
+            } );
+
+            handler.good = function() {
+                $( "#rename-photo-form" ).addClass( "has-success" );
+            };
+
+            handler.bad = function( data ) {
+                $( "#rename-photo-form" ).addClass( "has-error" );
+                var msg = "";
+                if ( data.photo === "not_found" ) {
+                    msg = "Опа, а такое фото не найдено! Что-то здесь не так.";
+                } else {
+                    msg = JSON.stringify( data );
+                }
+                var errorsHandler = require( "errors_handler" );
+                errorsHandler.error( msg );
+            };
         },
     });
 
--- www/template/register.js
+++ www/template/register.js
@@ -3,12 +3,12 @@ define(['handlebars.runtime'], function(Handlebars) {
 return templates['register'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<div class=\"container panel small fly\">\r\n    <p>\r\n        <form id=\"form-register\" onsubmit=\"return false;\" role=\"register\">\r\n            <h3 class=\"form-heading\">Регистрация</h3>\r\n            <p><input id=\"form-reg-name\" type=\"text\" class=\"form-control\" placeholder=\"Имя\" value=\""
+  return "<div class=\"container panel small fly\">\r\n  <p>\r\n    <form id=\"form-register\" onsubmit=\"return false;\" role=\"register\">\r\n      <h3 class=\"form-heading\">Регистрация</h3>\r\n      <p><input id=\"form-reg-name\" type=\"text\" class=\"form-control\" placeholder=\"Имя\" value=\""
     + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
-    + "\" required autofocus /></p>\r\n            <p><input id=\"form-reg-pasw\" type=\"password\" class=\"form-control\" placeholder=\"Пароль\" required /></p>\r\n            <p><input id=\"form-reg-mail\" type=\"email\" class=\"form-control\" placeholder=\"Почта\" value=\""
+    + "\" required autofocus /></p>\r\n      <p><input id=\"form-reg-pasw\" type=\"password\" class=\"form-control\" placeholder=\"Пароль\" required /></p>\r\n      <p><input id=\"form-reg-mail\" type=\"email\" class=\"form-control\" placeholder=\"Почта\" value=\""
     + alias3(((helper = (helper = helpers.email || (depth0 != null ? depth0.email : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"email","hash":{},"data":data}) : helper)))
-    + "\" required autofocus /></p>\r\n        <div id=\"form-reg-error\" class=\"alert alert-danger hidden\"><strong>Ошибка:</strong> "
+    + "\" required autofocus /></p>\r\n      <div id=\"form-reg-error\" class=\"alert alert-danger hidden\"><strong>Ошибка:</strong> "
     + alias3(((helper = (helper = helpers.error || (depth0 != null ? depth0.error : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"error","hash":{},"data":data}) : helper)))
-    + "</div>\r\n        <div id=\"form-reg-info\" class=\"alert alert-success hidden\">Регистрация прошла успешно, проверьте почту что-бы завершить регистрацию</div>\r\n        <button id=\"form-reg-btn\" class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Регистрация</button>\r\n      </form>\r\n    </p>\r\n</div>\r\n";
+    + "</div>\r\n      <div id=\"form-reg-info\" class=\"alert alert-success hidden\">Регистрация прошла успешно, проверьте почту что-бы завершить регистрацию</div>\r\n      <button id=\"form-reg-btn\" class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Регистрация</button>\r\n    </form>\r\n  </p>\r\n</div>\r\n";
 },"useData":true});
 });
