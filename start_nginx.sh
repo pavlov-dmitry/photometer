f88043571994c75645680b2130ed6d6fba094e92
pavlov-dmitry <voidwalker@mail.ru>
2015-11-26 10:35:52 +0300

    Теперь скрипт start_nginx перед запуском nginx выключает предыдущий процесс nginx

--- start_nginx.sh
+++ start_nginx.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# quiting nginx master process
+pid=$(ps ax | grep "nginx: master" | head -n1 | awk '{print $1}')
+sudo kill -s QUIT $pid
+
+# making config for nginx
 CURRENT_DIR=$(pwd)
 sed -e "s|{{dir}}|${CURRENT_DIR}/www/|g" -e "s|{{user}}|${USER}|g" ./etc/nginx.conf_tmpl > ./etc/nginx.conf
+# starting nginx
 sudo nginx -c ${CURRENT_DIR}/etc/nginx.conf
