9905a868272b2fecc964e251fd14540057e0e33e
pavlov-dmitry <voidwalker@mail.ru>
2015-12-28 17:38:08 +0300

    переделал пагинацию с меню на группу кнопок, так как у них есть состояние disable

--- www/template/pagination.js
+++ www/template/pagination.js
@@ -3,15 +3,15 @@ define(['handlebars.runtime'], function(Handlebars) {
 return templates['pagination'] = template({"1":function(container,depth0,helpers,partials,data) {
     var helper;
 
-  return "  <a class=\"item\" href=\""
+  return "  <a class=\"ui button\" href=\""
     + container.escapeExpression(((helper = (helper = helpers.prev || (depth0 != null ? depth0.prev : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"prev","hash":{},"data":data}) : helper)))
     + "\" aria-label=\"Предыдущая\">\n    <i class=\"angle double left icon\"></i>\n  </a>\n";
 },"3":function(container,depth0,helpers,partials,data) {
-    return "  <div class=\"item disabled\">\n    <i class=\"angle double left icon\"></i>\n  </div>\n";
+    return "  <div class=\"ui button disabled\">\n    <i class=\"angle double left icon\"></i>\n  </div>\n";
 },"5":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {};
 
-  return "  <a class=\"item"
+  return "  <a class=\"ui button "
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.active : depth0),{"name":"if","hash":{},"fn":container.program(6, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.disabled : depth0),{"name":"if","hash":{},"fn":container.program(8, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "\" "
@@ -32,11 +32,11 @@ return templates['pagination'] = template({"1":function(container,depth0,helpers
 },"12":function(container,depth0,helpers,partials,data) {
     var helper;
 
-  return "  <a class=\"item\" href=\""
+  return "  <a class=\"ui button\" href=\""
     + container.escapeExpression(((helper = (helper = helpers.next || (depth0 != null ? depth0.next : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"next","hash":{},"data":data}) : helper)))
     + "\" aria-label=\"Слудующая\">\n    <i class=\"angle double right icon\"></i>\n  </a>\n";
 },"14":function(container,depth0,helpers,partials,data) {
-    return "  <div class=\"item disabled\">\n    <i class=\"angle double right icon\"></i>\n  </div>\n";
+    return "  <div class=\"ui button disabled\">\n    <i class=\"angle double right icon\"></i>\n  </div>\n";
 },"16":function(container,depth0,helpers,partials,data) {
     return "";
 },"18":function(container,depth0,helpers,partials,data) {
@@ -77,7 +77,7 @@ return templates['pagination'] = template({"1":function(container,depth0,helpers
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=depth0 != null ? depth0 : {};
 
-  return "<div class=\"ui pagination icon menu\">\n"
+  return "<div class=\"ui small basic icon buttons\">\n"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.prev : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "")
     + "\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.pages : depth0),{"name":"each","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
