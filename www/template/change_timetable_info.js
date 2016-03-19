ee825fff98f53d54f64b52b31dac0e1c26c6a6e3
pavlov-dmitry <voidwalker@mail.ru>
2016-03-19 12:04:19 +0300

    Теперь описание изменения расписания проходит через markdown

--- www/template/change_timetable_info.js
+++ www/template/change_timetable_info.js
@@ -7,10 +7,10 @@ return templates['change_timetable_info'] = template({"1":function(container,dep
     + ((stack1 = (helpers.include || (depth0 && depth0.include) || helpers.helperMissing).call(depth0 != null ? depth0 : {},"timetable_value_info",depth0,{"name":"include","hash":{},"data":data})) != null ? stack1 : "")
     + "\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : {};
+    var stack1, alias1=depth0 != null ? depth0 : {};
 
   return "<div class=\"ui secondary segment description\">\n  "
-    + ((stack1 = ((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper))) != null ? stack1 : "")
+    + ((stack1 = (helpers.markdown || (depth0 && depth0.markdown) || helpers.helperMissing).call(alias1,(depth0 != null ? depth0.description : depth0),{"name":"markdown","hash":{},"data":data})) != null ? stack1 : "")
     + "\n</div>\n<div class=\"ui two column stackable grid\">\n  <div class=\"column\">\n    <div class=\"ui horizontal divider green header\">\n      <i class=\"plus icon\"></i>\n      Добавляются:\n    </div>\n    <div class=\"ui divided items\">\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.enable : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "    </div>\n  </div>\n  <div class=\"column\">\n    <div class=\"ui horizontal divider red header\">\n      <i class=\"minus icon\"></i>\n      Удаляются:\n    </div>\n    <div class=\"ui divided items\">\n"
