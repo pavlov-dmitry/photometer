c937ea82e44e83cc45b123b1139739283e8eb1f9
pavlov-dmitry <voidwalker@mail.ru>
2016-04-05 16:05:12 +0300

    Версия 1.0.0 (собираем в Debian 7)

--- www/template/build-template.sh
+++ www/template/build-template.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env sh
-nodejs /usr/local/bin/handlebars $1 --extension=hbs --amd --output ${1%.*}.js
+# debian 7 version
+handlebars $1 --extension=hbs --amd --output ${1%.*}.js
+# ubuntu 14.04 version
+# nodejs /usr/local/bin/handlebars $1 --extension=hbs --amd --output ${1%.*}.js
